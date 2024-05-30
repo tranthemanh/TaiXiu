5f85d27fa0a0111c433f7badff606343c8787a5a
tranthemanh <tranmanhhuha@gmail.com>
2024-05-30 13:40:54 +0700

    Lan 3

--- scripts.js
+++ scripts.js
@@ -5,20 +5,34 @@ let tongCuocTai = 0;
 let tongCuocXiu = 0;
 let timer;
 let demNguoc;
+let tongKQTai = 0;
+let tongKQXiu = 0;
 
 const tongSoTien = document.getElementById('tongtien');
 const dsLichSuKQ = document.getElementById('danhsachlichsu');
+const dsLichSuKQTai = document.getElementById('tongtai');
+const dsLichSuKQXiu = document.getElementById('tongxiu');
 
 // Tai trang thai game tu Local Storage
 function loadGame() {
     const luuTien = localStorage.getItem('gameTaiXiu');
     const luuKQ = localStorage.getItem('gameTaiXiu1');
+    const luuKQTai = localStorage.getItem('gameTaiXiu2');
+    const luuKQXiu = localStorage.getItem('gameTaiXiu3');
     if (luuTien !== null) {
         tongTien = parseInt(luuTien);
-        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toFixed(2)}`;
+        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toLocaleString('vi-VN')} đ`;
     }
     if (luuKQ !== null) {
         dsLichSuKQ.innerHTML = luuKQ;
+    }
+    if (luuKQTai !== null) {
+        tongKQTai = parseInt(luuKQTai);
+        dsLichSuKQTai.innerText = `Tổng kết quả Tài: ${tongKQTai}`;
+    }
+    if (luuKQXiu !== null) {
+        tongKQXiu = parseInt(luuKQXiu);
+        dsLichSuKQXiu.innerText = `Tổng kết quả Xỉu: ${tongKQXiu}`;
     }
 }
 
@@ -26,11 +40,13 @@ function loadGame() {
 function saveGame() {
     localStorage.setItem('gameTaiXiu', tongTien);
     localStorage.setItem('gameTaiXiu1', dsLichSuKQ.innerHTML);
+    localStorage.setItem('gameTaiXiu2', tongKQTai);
+    localStorage.setItem('gameTaiXiu3', tongKQXiu);
 }
 
 // Bắt đầu game
 function batDauDemThoiGian() {
-    let thoiGian = 30;
+    let thoiGian = 15;
     demNguoc = setInterval(() => {
         thoiGian--;
         document.getElementById('thoigian').innerText = `Thời gian còn lại: ${thoiGian} giây`;
@@ -55,7 +71,15 @@ function thietLapLaiGame() {
     document.getElementById('thoigian').innerText = "Thời gian còn lại: 15 giây";
     batDauGame();
     saveGame();
+    tongSoTaiVaXiu();
 }
+
+// //Xóa lịch sử trò chơi
+// function xoaLichSu() {
+//     localStorage.removeItem('gameTaiXiu1');
+//     dsLichSuKQ.innerHTML = 0;
+//
+// }
 
 // Kiểm tra tiền cược Tài
 document.getElementById('cuoctai').addEventListener('click', function () {
@@ -65,7 +89,7 @@ document.getElementById('cuoctai').addEventListener('click', function () {
     } else {
         tongTien -= tienCuocTai;
         tongCuocTai += tienCuocTai;
-        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toFixed(2)}`;
+        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toLocaleString('vi-VN')} đ`;
         document.getElementById('tongcuoctai').innerText = `Tổng cược Tài: ${tongCuocTai}`;
     }
 });
@@ -78,17 +102,41 @@ document.getElementById('cuocxiu').addEventListener('click', function () {
     } else {
         tongTien -= tienCuocXiu;
         tongCuocXiu += tienCuocXiu;
-        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toFixed(2)}`;
+        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toLocaleString('vi-VN')} đ`;
         document.getElementById('tongcuocxiu').innerText = `Tổng cược Xỉu: ${tongCuocXiu}`;
     }
 });
+
+function tongSoTaiVaXiu() {
+    let tongKQTai1 = 0;
+    let tongKQXiu2 = 0;
+
+    //kiểm tra tổng số lần ra tài và xỉu
+    let list = dsLichSuKQ.children
+    console.log(dsLichSuKQ.children)
+    for (let i = 0; i < list.length; i++) {
+        let kq = list[i].innerText.split(' ')
+        let ch = 0
+        if (kq.length === 11) {
+            ch = parseInt(kq[kq.length - 3])
+        } else {
+            ch = parseInt(kq[kq.length - 1])
+        }
+        if (ch < 11) {
+            tongKQXiu2++;
+        } else {
+            tongKQTai1++;
+        }
+    }
+    dsLichSuKQTai.innerText = `Tổng kết quả Tài: ${tongKQTai1}`;
+    dsLichSuKQXiu.innerText = `Tổng kết quả Xỉu: ${tongKQXiu2}`;
+}
 
 // Tung Xúc Xắc
 function tungXucXac() {
     let xucXac1 = 0;
     let xucXac2 = 0;
-    let tongKQTai = 0;
-    let tongKQXiu = 0;
+
     if (xx1 > 0) {
         xucXac1 = +xx1;
         xucXac2 = +xx2;
@@ -106,30 +154,24 @@ function tungXucXac() {
 
     let hienThiKetQua = `Kết quả: ${xucXac1} + ${xucXac2} + ${xucXac3} = ${ketQua}`;
     let ketQuaTai = "", ketQuaXiu = "";
-    if (ketQua === 3 || ketQua === 18) {
-        hienThiKetQua += " Bạn thua!";
-    } else if (ketQua >= 4 && ketQua <= 10) {
+    if (ketQua >= 3 && ketQua <= 10) {
         if (tongCuocXiu > 0) {
             tongTien += tongCuocXiu + tongCuocXiu * 0.98;
             ketQuaXiu += " Bạn thắng!";
-            tongKQXiu++;
         }
         if (tongCuocTai > 0) {
             ketQuaTai += " Bạn thua!";
-            tongKQTai++;
         }
-    } else if (ketQua >= 11 && ketQua <= 17) {
+    } else if (ketQua >= 11 && ketQua <= 18) {
         if (tongCuocTai > 0) {
             tongTien += tongCuocTai + tongCuocTai * 0.98;
             ketQuaTai += " Bạn thắng!";
-            tongKQTai++;
         }
         if (tongCuocXiu > 0) {
             ketQuaXiu += " Bạn thua!";
-            tongKQXiu++;
         }
     }
-    tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toFixed(2)}`;
+    tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toLocaleString("vi-VN")} đ`;
     document.getElementById('ketqua').innerText = hienThiKetQua + " " + ketQuaTai + " " + ketQuaXiu;
 
     // Thêm vào lịch sử
@@ -137,8 +179,6 @@ function tungXucXac() {
     const listItem = document.createElement('li');
     listItem.textContent = hienThiKetQua + " " + ketQuaTai + " " + ketQuaXiu;
     dsLichSuKQ.appendChild(listItem);
-    document.getElementById('tongtai').innerHTML = `Tổng kết quả Tài: ${tongKQTai}`;
-    document.getElementById('tongxiu').innerHTML = `Tổng kết quả Tài: ${tongKQXiu}`;
 
     tongCuocTai = 0;
     tongCuocXiu = 0;
@@ -172,7 +212,7 @@ document.getElementById('naptien').addEventListener('click', function () {
     const amount = parseFloat(document.getElementById('sotiennap').value);
     if (amount > 0) {
         tongTien += amount;
-        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toFixed(2)}`;
+        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toLocaleString('vi-VN')} đ`;
     }
     document.getElementById('naptien-chinh').classList.add('hidden');
 });
@@ -191,7 +231,7 @@ document.getElementById('ruttien').addEventListener('click', function () {
     const amount = parseFloat(document.getElementById('sotienrut').value);
     if (amount > 0 && amount <= tongTien) {
         tongTien -= amount;
-        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toFixed(2)}`;
+        tongSoTien.innerText = `Số tiền hiện có: ${tongTien.toLocaleString('vi-VN')} đ`;
     } else if (amount > tongTien) {
         alert("Bạn rút quá số tiền hiện có!");
     } else if (amount <= 0) {
